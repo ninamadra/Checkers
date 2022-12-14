5323c7319874ad1061193b464c887a04ed0c8ed9
nina <ninamadra@gmail.com>
2022-12-14 16:48:23 +0100

    stworzenie szkieletu singletona serwera (double-checked locking i prowizoryczny run)

---
+++ src/main/java/org/example/Server/Server.java
@@ -0,0 +1,41 @@
+package org.example.Server;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private static Server instance = null;
+    private ServerSocket serverSocket;
+
+
+    public Server() {
+    }
+
+    public static Server getInstance() {
+        if(instance == null) {
+            synchronized (Server.class) {
+                if(instance == null) {
+                    instance = new Server();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+    public void run() throws InterruptedException {
+
+        try {
+            serverSocket = new ServerSocket(2137);
+
+            while(true) {
+                Socket socket = serverSocket.accept();
+            }
+        }
+        catch(IOException ex) {}
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        getInstance().run();
+    }
+}

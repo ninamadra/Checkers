1aff0ff4182db5902f0fa4e70fa6cdf532552bd1
MichalZurawski02 <90862515+MichalZurawski02@users.noreply.github.com>
2022-12-15 16:39:47 +0100

    Revert "Model"

--- src/main/java/org/example/model/factory/AbstractVarFactory.java
+++
@@ -1,9 +0,0 @@
-package org.example.model.factory;
-
-import org.example.model.board.AbstractBoard;
-import org.example.model.rules.AbstractRules;
-
-public interface AbstractVarFactory {
-    AbstractBoard createBoard();
-    AbstractRules createRules();
-}
